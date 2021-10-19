e716d454d93604fdffbb2c7683b83894d9d5574e
JoshDaos <joshuadaos1998@gmail.com>
2021-10-19 11:42:00 -0700

    refactor data rendering & added card comp

--- react-app/src/components/card-list/card-list.component.jsx
+++ react-app/src/components/card-list/card-list.component.jsx
@@ -1,8 +1,16 @@
-import './card-list.styles.css'
+import './card-list.styles.css';
 
+import { Card } from '../card/card.component';
+
+
+// responsible of rendering monsters
 export const CardList = props => {
     console.log(props)
     return(
-        <div className='card-list'>{props.children}</div>
+        <div className='card-list'>
+            { props.monsters.map(monster => (
+                <Card key={monster.id} monster={monster} />
+            ))}
+        </div>
     )
 }
