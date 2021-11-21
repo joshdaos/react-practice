6de39174a56c31bb03a736a09fc5cc83d6c59bdc
joshdaos <joshuadaos1998@gmail.com>
2021-11-21 12:04:31 -0800

    testing

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-
+// imports
 import { CardList } from './components/card-list/card-list.component';
 
 import { SearchBox } from './components/search-box/search-box.component';
